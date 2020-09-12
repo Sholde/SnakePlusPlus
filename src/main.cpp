e03973469db953b82e8daf3ca1b5e5b169cae9c8
Sholde <exshode@gmail.com>
2020-09-12 18:16:03 +0200

    link sfml library to project and bash script for compile and run

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+using namespace std;
 
-int main(int argc, const char** argv) {
-    return 0;
+int main(int argc, char* argv[]) {
+  cout << "Version " << "." << endl;
+
+  sf::Window screen(sf::VideoMode(800, 600), "myproject");
+  bool running = true;
+  while (running) {
+    screen.display();
+  }
 }
