ba86515ee0f12712268f9e5ef4ea390145d03c5b
Sholde <exshode@gmail.com>
2020-09-12 18:44:50 +0200

    mini example and includes directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-using namespace std;
+
+#define RESTART_KEY R
 
 int main(int argc, char* argv[]) {
-  cout << "Version " << "." << endl;
 
-  sf::Window screen(sf::VideoMode(800, 600), "myproject");
-  bool running = true;
-  while (running) {
-    screen.display();
+  sf::RenderWindow  window(sf::VideoMode(800, 600), "SnakePlusPlus");
+
+  while (window.isOpen()) {
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::RESTART_KEY))
+				window.close();
+		}
+
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+
+		// draw everything here...
+		// window.draw(...);
+
+		// end the current frame
+		window.display();
   }
+	return 0;
 }
